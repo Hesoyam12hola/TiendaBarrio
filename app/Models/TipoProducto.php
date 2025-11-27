437f7ec7cd9e4a63705c17ce1b243237b80c2e0f
justin <justinavila2507@gmail.com>
2025-11-27 14:40:09 -0500

    se editan modelos

--- app/Models/TipoProducto.php
+++ app/Models/TipoProducto.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoProducto extends Model
 {
+    protected $fillable = ['nombre', 'descripcion'];
+
     public function productos()
-{
-    return $this->hasMany(Producto::class);
+    {
+        return $this->hasMany(Producto::class);
+    }
 }
 
-}
