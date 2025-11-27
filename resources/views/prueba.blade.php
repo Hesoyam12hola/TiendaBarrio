6ac9b1038e80e0ba102571b077decd4b0d53880d
justin <justinavila2507@gmail.com>
2025-11-27 14:51:30 -0500

    se genera una priueba de bootstrap

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-5">
+    <h1 class="text-center text-primary">Bootstrap funciona 🎉</h1>
+
+    <button class="btn btn-success mt-3">Botón Bootstrap</button>
+</div>
+@endsection
